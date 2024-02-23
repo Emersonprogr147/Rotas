e5cb2c30e845b79c9bfec9671e25efd6cbafeed6
Emerson dos Santos <emerson.santos@goesnicoladelli.net>
2024-02-23 16:15:31 -0300

    added code Routes

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/','PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos') ; 
+Route::get('/contato', 'ContatoController@contato')->name('site.contato'); 
+Route::get('/login', function(){  return 'Login'  ; }); 
+/*app*/
+
+Route::prefix('/app')->group(function(){
+Route::get('/clientes', function(){ return 'Clientes' ; } )->name('app.clientes'); 
+Route::get('/fornecedores', function() { return  'Fornecedores'; } )->name('app.fornecedores'); 
+Route::get('/produtos', function(){ return ' Produtos';}  )->name('app.produtos'); 
+
+
+});
+
+Route::get('/rota1', function (){
+  echo 'Rota 01';
+})->name('site.rota1');
+
+Route::get('/rota2', function (){
+    echo 'Rota 02';
+})->name('site.rota2');
+
